180a5b7533a08c73226a80ae4e0b395bee538518
Lucas Carvalho <lucascarvalho34@gmail.com>
2022-09-10 17:37:12 -0300

    chore: add jest env to eslint config

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ export default {
   ],
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '^.+\\.ts?$': 'ts-jest'
   }
   // All imported modules in your tests should be mocked automatically
   // automock: false,
